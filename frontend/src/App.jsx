a4ccda4ffc141c2bffbf8d8c402ed1d99fc13a79
KaryampudiMadhav <karyampudimadhav@gmail.com>
2025-08-31 21:20:12 +0530

    The Frotend Ui is Completed added charts.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import TransactionPage from "./pages/TransactionPage";
+import Header from "./components/Header";
+import SignUpPage from "./pages/SignUp";
+
+function App() {
+  const authUser = true;
+  return (
+    <>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
